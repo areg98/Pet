be07602681be5539e21d51d084d4319fad00c2f5
areg98 <hovakimyan.ah@gmail.com>
2026-02-24 22:39:59 +0400

    Start

--- src/main/java/pojo/Pet.java
+++ src/main/java/pojo/Pet.java
@@ -1,4 +1,20 @@
 package pojo;
 
+import java.util.ArrayList;
+
 public class Pet {
+
+    public class Category{
+        public int id;
+        public String name;
+    }
+
+    public class Root{
+        public int id;
+        public Category category;
+        public String name;
+        public ArrayList<Object> photoUrls;
+        public ArrayList<Object> tags;
+        public String status;
+    }
 }
--- src/test/java/PetTest.java
+++ src/test/java/PetTest.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
+public class PetTest {
 
-public class Pet {
+
 }
